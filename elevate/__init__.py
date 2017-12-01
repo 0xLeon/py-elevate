bce687e29644adc3b9db2aebe513efba535ec84f
Stefan <Stefan_Hahn1@gmx.net>
2017-12-01 11:26:28 +0100

    Indent using tabs

--- elevate/__init__.py
+++ elevate/__init__.py
@@ -10,36 +10,36 @@ from builtins import str as text
 __version__ = '0.0.0'
 
 def elevate(args=None):
-    """Tries to rerun the current script with admin rights"""
+	"""Tries to rerun the current script with admin rights"""
 
-    if not is_admin():
-        if args is None:
-            args = sys.argv
+	if not is_admin():
+		if args is None:
+			args = sys.argv
 
-        runas(sys.executable, args)
+		runas(sys.executable, args)
 
-        return False
+		return False
 
-    return True
+	return True
 
 def runas(executable, args=None):
-    params = []
+	params = []
 
-    if args is not None:
-        for arg in args:
-            if '"' in arg:
-                arg = '"' + arg.replace('"', r'\"') + '"'
-            elif ' ' in arg:
-                arg = '"' + arg + '"'
+	if args is not None:
+		for arg in args:
+			if '"' in arg:
+				arg = '"' + arg.replace('"', r'\"') + '"'
+			elif ' ' in arg:
+				arg = '"' + arg + '"'
 
-            params.append(arg)
+			params.append(arg)
 
-    res = ctypes.windll.shell32.ShellExecuteW(None, "runas", text(executable), text(' '.join(params)), None, 1)
+	res = ctypes.windll.shell32.ShellExecuteW(None, "runas", text(executable), text(' '.join(params)), None, 1)
 
-    if res <= 32:
-        raise WindowsError("ShellExecute returned error code %d" % res)
+	if res <= 32:
+		raise WindowsError("ShellExecute returned error code %d" % res)
 
 def is_admin():
-    """Checks if the script is run with full admin rights"""
+	"""Checks if the script is run with full admin rights"""
 
-    return ctypes.windll.shell32.IsUserAnAdmin() == 1
+	return ctypes.windll.shell32.IsUserAnAdmin() == 1
